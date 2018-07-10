bb122824bde9be12c3b128154093d432a61f4dc6
Patryk Srednicki <patryk.srednicki@acaisoft.com>
2018-07-10 15:26:43 +0200

    Interface update

--- src/azure-map/interfaces/am-config.ts
+++ src/azure-map/interfaces/am-config.ts
@@ -1,26 +1,2 @@
 
 declare type AmConfig = ServiceOptions & CameraOptions & StyleOptions & UserInteractionOptions;
-  // serviceOptions: ServiceOptions;
-  // cameraOptions: CameraOptions;
-  // styleOptions: StyleOptions;
-  // userInteractionsOptions: IserInteractionsOptions;
-  // 'disable-telemetry'?: boolean; // default: false
-  // 'session-id'?: string; // default: UUID.generate()
-  // 'subscription-key': string; // default: undefined
-  // bearing?: number; // default: 0
-  // center?: Position; // default: new Position(0,0)
-  // maxZoom?: number; // default: 20
-  // minZoom?: number; // default: 1
-  // pitch?: number; // default: 0
-  // zoom?: number;  // default: 1
-  // language?: string;  // default: "NGT"
-  // style?: 'vector';  // default: 'vector'
-  // view?: 'Unified';  // default: 'Unified'
-  // dblClickZoomInteraction?: boolean; // default: true
-  // dragPanInteraction?: boolean; // default: true
-  // dragRotateInteraction?: boolean; // default: true
-  // interactive?: boolean; // default: true
-  // keyboardInteraction?: boolean; // default: true
-  // scrollZoomInteraction?: boolean; // default: true
-  // touchInteraction?: boolean; // default: true
-
