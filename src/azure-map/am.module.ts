fa1291a7f65142b5984b2efd97a3d0803f38cbb8
Maciej Sawicki <humberd2@gmail.com>
2018-07-31 10:35:46 +0200

    Exported map component (#13)

--- src/azure-map/am.module.ts
+++ src/azure-map/am.module.ts
@@ -1,14 +1,11 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AtlasMapComponent } from './atlas-map/atlas-map.component';
 import { AtlasPopupDirective } from './directives/atlas-popup.directive';
-import {BrowserModule} from '@angular/platform-browser';
-
 
 @NgModule({
   imports: [
-    CommonModule,
-    BrowserModule
+    CommonModule
   ],
   providers: [],
   declarations: [AtlasMapComponent, AtlasPopupDirective],
